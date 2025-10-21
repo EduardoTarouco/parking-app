5f72547e9ce19baba8d942f0a80d300bcd041130
Eduardo Duarte Tarouco <eduardotarouco0@gmail.com>
2025-10-21 16:49:24 -0300

    feat: adiciona botão de signOut

--- screens/app/home/index.jsx
+++ screens/app/home/index.jsx
@@ -1,3 +1,5 @@
+import { signOutUser } from "@/services/UserFirebaseService";
+import { Button, ButtonText } from "@/components/ui/button";
 import { View, Text } from "react-native";
 
 export const Home = () => {
@@ -5,6 +7,12 @@ export const Home = () => {
   return (
     <View className="flex-1 items-center justify-center bg-green-500">
       <Text className="text-xl font-bold text-white">Home screen working!</Text>
+
+      <Button variant="solid" size="md" action="negative"
+        onPress={() => {signOutUser()}}
+      >
+        <ButtonText>Sair</ButtonText>
+      </Button>
     </View>
   );
 }
